f713fc1369f87c30fc87031abfff88bcd5992412
ViniciusHallal <Vinicius.vmhallal@gmail.com>
2026-01-08 23:15:06 -0300

    Class 27

---
+++ Class27/Cake.cs
@@ -0,0 +1,30 @@
+﻿namespace Class27
+{
+    public class Cake
+    {
+        public string Flavor;
+        public string Filling;
+        public string Coverage;
+        public int Layers;
+        public string Size;
+
+        public Cake(string Flavor, string Filling, string Coverage, int Layers, string Size)
+        { 
+            this.Flavor = Flavor;
+            this.Filling = Filling;
+            this.Coverage = Coverage;
+            this.Layers = Layers;
+            this.Size = Size;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"\n--- CAKE DETAILS ---");
+            Console.WriteLine($"Flavor: {Flavor}");
+            Console.WriteLine($"Filling: {Filling}");
+            Console.WriteLine($"Coverage: {Coverage}");
+            Console.WriteLine($"Layers: {Layers}");
+            Console.WriteLine($"Size: {Size}");
+        }
+    }
+}
---
+++ Class27/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Class27;
+
+public class Program
+{
+    public static void Main()
+    {
+        Cake cakeChocolate = new Cake("Chocolate", "Coconut", "Chocolate", 3, "Medium");
+        Cake cakeLemon = new Cake("Lemon", "White Chocolate", "Lemon", 2, "Small");
+
+        cakeChocolate.ShowInfo();
+        cakeLemon.ShowInfo();
+    }
+}
